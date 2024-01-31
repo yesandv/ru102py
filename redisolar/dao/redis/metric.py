be6a8bd7fc7becce0b207a2a10b5d9ea7d7e3369
yesandv <grc@protonmail.com>
2024-01-31 20:26:36 +0300

    Update the insert_metric method

--- redisolar/dao/redis/metric.py
+++ redisolar/dao/redis/metric.py
@@ -125,6 +125,7 @@ class MetricDaoRedis(MetricDaoBase, RedisDaoBase):
         pipeline.zadd(
             metric_key, mapping={str(MeasurementMinute(value, minute_of_day)): minute_of_day}
         )
+        pipeline.expire(metric_key, METRIC_EXPIRATION_SECONDS)
 
     def get_recent(self, site_id: int, unit: MetricUnit, time: datetime.datetime,
                    limit: int, **kwargs) -> Deque[Measurement]:
